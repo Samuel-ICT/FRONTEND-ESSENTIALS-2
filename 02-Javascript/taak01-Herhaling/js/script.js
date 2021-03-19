9cc1924290410cf7de6fa335881903ab3f68574e
Samuel Adu <samuel.adu@student.rocva.nl>
2021-03-19 12:06:49 +0100

    herhaling

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,20 @@
 let strNaam;
+strNaam = prompt ("Hoe heet je?");
+
 let boolNaamKlopt;
+boolNaamKlopt = confirm("Heet je " + strNaam + "?");
+
+if(boolNaamKlopt == true){ 
+    alert("Welkom "+strNaam +"!");
+}
+else { 
+    strNaam = prompt("Nog een keer, hoe heet je?");
+    boolNaamKlopt = confirm("Heet je "+strNaam+"?");
+    if(boolNaamKlopt == true){ 
+    alert("Welkom "+strNaam +"!");
+    }
+    else{
+    alert("Ok, alsnog welkom !");   
+    }
+}
+
