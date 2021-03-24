627b59a7c0d746527f946c50a61e0618647a6b76
Samuel Adu <samuel.adu@student.rocva.nl>
2021-03-24 14:48:36 +0100

    parameter

---
+++ 02-Javascript/taak04-Functions-Parameters/js/script.js
@@ -0,0 +1,11 @@
+
+vulGetal1 = prompt ("Vul een getal in?");
+
+
+vulGetal2 = prompt ("Vul een getal in?");
+
+function vermenigVuldigen(vulGetal1, vulGetal2){
+    let som = vulGetal1 * vulGetal2;
+    return som;
+}
+console.log(vermenigVuldigen(vulGetal1,vulGetal2));
