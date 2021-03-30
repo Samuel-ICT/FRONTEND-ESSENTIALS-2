b3985dce18911a9e6662d0b12071d45d9815c208
Samuel Adu <samuel.adu@student.rocva.nl>
2021-03-30 17:57:59 +0200

    game

--- 02-Javascript/taak06-Functions-Samenvatting/js/script.js
+++ 02-Javascript/taak06-Functions-Samenvatting/js/script.js
@@ -1,4 +1,6 @@
-var score = 0;
+var huidigeKleur = "white";
+var score = 0; 
+
 let getal1;
 
 function makeRandomNumber(){
@@ -12,3 +14,74 @@ function showRandomNumber1(){
 }
 
 getal1 = showRandomNumber1();
+
+let getal2;
+
+function makeRandomNumber2(){
+    return Math.floor(Math.random()*99+1);
+}
+
+function showRandomNumber2(){
+    let randomGetal = makeRandomNumber2()
+    document.querySelector(".getal2").innerHTML = randomGetal;
+    return randomGetal;
+}
+
+getal2 = showRandomNumber2();
+
+function gameScore(){
+     let antwoordScore = score ;
+     antwoordScore = (score * 1) + 1;
+    return antwoordScore;
+
+}
+score = gameScore();
+
+function gameScore2(){
+    let antwoordScore = score ;
+    antwoordScore = (score * 1) - 1;
+   return antwoordScore;
+
+}
+score = gameScore2();
+
+function geefAntwoord(Antwoord){
+    getal1 = document.querySelector(".getal1").innerHTML;
+    getal2 = document.querySelector(".getal2").innerHTML;
+    score = document.querySelector(".score").innerHTML;
+    if(getal1 > getal2&&Antwoord=='hoger'){
+        score =  gameScore(); 
+        document.querySelector(".score").innerHTML= score;
+        document.querySelector("body").style.backgroundColor = "Green" ;
+        getal1 = document.querySelector(".getal1").innerHTML = showRandomNumber1();
+        getal2 = document.querySelector(".getal2").innerHTML = showRandomNumber2();
+    } else if(getal1<getal2&&Antwoord=='hoger'){
+        score = gameScore2() ; 
+        document.querySelector(".score").innerHTML= score;
+        document.querySelector("body").style.backgroundColor = "Red" ;
+        getal1 = document.querySelector(".getal1").innerHTML = showRandomNumber1();
+        getal2 = document.querySelector(".getal2").innerHTML = showRandomNumber2(); 
+    }
+    if(getal1<getal2&&Antwoord=='lager'){
+        score = gameScore() ; 
+        document.querySelector(".score").innerHTML= score;
+        document.querySelector("body").style.backgroundColor = "Green" ;
+        getal1 = document.querySelector(".getal1").innerHTML = showRandomNumber1();
+        getal2 = document.querySelector(".getal2").innerHTML = showRandomNumber2(); 
+    }
+    else if(getal1>getal2&&Antwoord == 'lager'){
+        score = gameScore2() ; 
+        document.querySelector(".score").innerHTML= score;
+        document.querySelector("body").style.backgroundColor = "Red" ;
+        getal1 = document.querySelector(".getal1").innerHTML = showRandomNumber1();
+        getal2 = document.querySelector(".getal2").innerHTML = showRandomNumber2(); 
+    }
+    
+}
+
+
+
+
+
+
+
