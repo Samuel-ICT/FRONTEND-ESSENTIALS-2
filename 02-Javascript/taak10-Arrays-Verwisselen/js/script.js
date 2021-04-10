dd50b5119130b3cd728ba46e3c9ade05f2ab0a52
Samuel Adu <samuel.adu@student.rocva.nl>
2021-04-10 19:04:54 +0200

    verwisselen button

--- 02-Javascript/taak10-Arrays-Verwisselen/js/script.js
+++ 02-Javascript/taak10-Arrays-Verwisselen/js/script.js
@@ -16,3 +16,34 @@ document.querySelector(".film4-ranking").innerHTML = 4;
 document.querySelector(".film4-naam").innerHTML = topFilms[3];
 document.querySelector(".film5-ranking").innerHTML = 5;
 document.querySelector(".film5-naam").innerHTML = topFilms[4];
+
+
+
+function moveup(nummer){
+ bewaren = topFilms[nummer -1];
+ verwijderen = topFilms.splice(nummer -1, 1);
+ toevoegen = topFilms.splice(nummer -2, 0, bewaren);
+ console.log(topFilms);
+ beweegButton();
+}
+
+function movedown(nummer){
+ bewaren = topFilms[nummer -1];
+ verwijderen = topFilms.splice(nummer-1,1); 
+ toevoegen = topFilms.splice(nummer,0,bewaren);
+ beweegButton();
+}
+function resetList(){
+    document.querySelector(".film1-naam").innerHTML = "The Godfather";
+    document.querySelector(".film2-naam").innerHTML = "The Shawshank Redemption";
+    document.querySelector(".film3-naam").innerHTML = "Schindler's List";
+    document.querySelector(".film4-naam").innerHTML = "Raging Bull";
+    document.querySelector(".film5-naam").innerHTML = "Casablanca";
+}
+function beweegButton(){
+    document.querySelector(".film1-naam").innerHTML = topFilms[0];
+    document.querySelector(".film2-naam").innerHTML = topFilms[1];
+    document.querySelector(".film3-naam").innerHTML = topFilms[2];
+    document.querySelector(".film4-naam").innerHTML=  topFilms[3];
+    document.querySelector(".film5-naam").innerHTML = topFilms[4];  
+}
