8dc2020d1d1ac13ccb9d85e57d778c8a6fbba4c3
Samuel Adu <samuel.adu@student.rocva.nl>
2021-04-14 14:00:41 +0200

    spel

--- 02-Javascript/taak14-Loops-Strings/js/script.js
+++ 02-Javascript/taak14-Loops-Strings/js/script.js
@@ -1,9 +1,7 @@
-function outputLetters(str){
-    for (let i = 0; i < str.length; i++) {
-        const element = array[i];
-        
+voorbeeldString = prompt("Vul je naam in");
+//function outputLetters(voorbeeldString){
+    for (i = 0; i < voorbeeldString.length; i++) {
+        console.log(voorbeeldString[i]); 
+          
     }
-}
-
-var voorbeeldString = "Dit is een array van letters";
-console.log(voorbeeldString.length); // geeft terug: 
+//}
