ea9fe95c35bb7097ce6ce7d8fb1853dcf31b1858
Samuel Adu <samuel.adu@student.rocva.nl>
2021-04-15 10:43:19 +0200

    product

--- 02-Javascript/taak16-Loops-in-Loops/js/script.js
+++ 02-Javascript/taak16-Loops-in-Loops/js/script.js
@@ -6,3 +6,19 @@ function rondaf(getal, aantalplekkenachterkomma){
     const x = Math.pow(10, aantalplekkenachterkomma);
     return Math.round(getal * x)/x;
 }
+var totaalPrijs = 0;
+for (let i = 0; i < arrProductPrijs.length; i++) {
+ for (let j = 0; j < arrProductKorting.length; j++) {
+     arrProductPrijs[i] = arrProductPrijs[i] * arrProductKorting[j];
+     totaal = arrProductPrijs[i];
+     afgerondePrijs = rondaf (totaal,2)
+     
+     
+ }   
+ console.log("Product " + i + ": " + afgerondePrijs);
+ totaalPrijs += afgerondePrijs; 
+ 
+}
+
+
+console.log("Totaal" + ":" + totaalPrijs)
